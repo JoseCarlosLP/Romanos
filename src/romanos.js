9c0b6b84feb073e64714b0485e10d5e43ca48268
Jose Carlos Lozada Peralta <jose.lozada@ucb.edu.bo>
2023-03-20 15:35:22 -0400

    Se realizo la implementacion de la funcionalidad de retornar el numero romano hasta 8

--- src/romanos.js
+++ src/romanos.js
@@ -3,10 +3,13 @@ function romanosHasta9(num){
     if(num==4) {
         romano="IV"; num=num-4;
     }
+    if(num>=5) {
+        romano="V"; num=num-5;
+    }
     for(let i=1;i<=num;i++){
         romano=romano+"I";
     }
     return romano;
   }
 
-  export default romanosHasta9;
+export default romanosHasta9;
--- src/romanos.spec.js
+++ src/romanos.spec.js
@@ -13,6 +13,15 @@ describe("Romanos", () => {
   it("numero romano para el 4", () => {
     expect(romanosHasta9(4)).toEqual("IV");
   });
+  it("numero romano para el 5", () => {
+    expect(romanosHasta9(5)).toEqual("V");
+  });
+  it("numero romano para el 6", () => {
+    expect(romanosHasta9(6)).toEqual("VI");
+  });
+  it("numero romano para el 8", () => {
+    expect(romanosHasta9(8)).toEqual("VIII");
+  });
 });
 
 
